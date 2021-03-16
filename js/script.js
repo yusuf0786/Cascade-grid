2370f120b210538bbe91c64ab9927aae6c09d87a
yusuf0786 <yusssuf0786@gmail.com>
2021-03-16 18:05:09 +0530

    another commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-
-$("#substract").click(function(){
-    $("#quantity").css({
-        'value':'-=1'
-    });
-});
+var a = 0;
+var b = 0;
+function minus(){
+    document.getElementById("quantity").value=a;
+    a=a-1;
+}
+function addition(){
+    document.getElementById("quantity").value=b;
+    b=b+1;
+}
